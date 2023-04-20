65897c2da3271bd9132a382854003201080e3d27
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-04-20 13:31:57 +0300

    show data from db

--- django_todo/admin.py
+++ django_todo/admin.py
@@ -6,3 +6,8 @@ from .models import Todo
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ["title", "checked"]
+    actions = ["mark_done"]
+
+    @admin.action(description="Mark as Done")
+    def mark_done(self, request, queryset):
+        queryset.update(checked=True)
