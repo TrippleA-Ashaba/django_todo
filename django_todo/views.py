086817754b86bea2e791595410fdb76c714f3c5c
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-04-20 13:04:13 +0300

    add todo model

--- django_todo/views.py
+++ django_todo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Todo
 
 
 # Create your views here.
-class Home(TemplateView):
+class Home(ListView):
     template_name = "index.html"
+    context_object_name = "todos"
+    model = Todo
