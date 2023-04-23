0f36992896212161ee6cc8da195e7faf976a6b9e
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-04-23 14:54:55 +0300

    add more environ variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-85$rq60(r6a-a%mvv1e=sq(+7m1eyml6+*s45#9)@8rne5(zwz"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
