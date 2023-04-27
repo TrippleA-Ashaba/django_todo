88f7d4b05a3dc0e7ba10461de1e3e9f59b12a386
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-04-27 13:53:22 +0300

    complete todo app

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import AddTodo, DeleteTodo, Home, test_view, toggle_todo
+from .views import AddTodo, DeleteTodo, Home, toggle_todo
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("add", AddTodo.as_view(), name="add_todo"),
     path("delete/<str:id>", DeleteTodo.as_view(), name="delete_todo"),
-    path("test", test_view, name="test"),
     path("todos/<int:pk>/toggle/", toggle_todo, name="toggle_todo"),
 ]
--- django_todo/views.py
+++ django_todo/views.py
@@ -1,16 +1,14 @@
+from django.db.models import Case, Count, F, FloatField, Value, When
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
-    ListView,
-    TemplateView,
     DeleteView,
-    UpdateView,
+    ListView,
 )
 
 from .forms import TodoForm
 from .models import Todo
-from django.db.models import Count, F, FloatField, Case, When, Value
 
 
 # Create your views here.
@@ -27,30 +25,6 @@ class Home(ListView):
         percentage_done = (done_count / total_count) * 100 if total_count > 0 else 0
         context["percentage_done"] = percentage_done
         return context
-
-
-def test_view(request):
-    # todo_queryset = Todo.objects.annotate(
-    #     done_count=Count("id", filter=F("done")),
-    #     total_count=Count("id"),
-    #     percentage_done=F("done_count") / F("total_count") * 100,
-    # ).order_by("done", "-date_created")
-
-    todo_queryset = Todo.objects.all().order_by("done", "-date_created")
-
-    total_count = todo_queryset.count()
-    done_count = todo_queryset.aggregate(done_count=Count("id", filter=F("done"))).get(
-        "done_count", 0
-    )
-
-    percentage_done = (done_count / total_count) * 100 if total_count > 0 else 0
-
-    context = {
-        "todos": todo_queryset,
-        "percentage_done": percentage_done,
-    }
-
-    return render(request, "index.html", context)
 
 
 class AddTodo(CreateView):
@@ -71,21 +45,3 @@ def toggle_todo(request, pk):
     todo.done = not todo.done
     todo.save()
     return redirect("home")
-
-
-# class ToggleTodoView(UpdateView):
-#     model = Todo
-#     fields = ['done']
-#     success_url = reverse_lazy('todo_list')
-
-#     def form_valid(self, form):
-#         # Toggle the 'done' field value
-#         self.object = form.save(commit=False)
-#         self.object.done = not self.object.done
-#         self.object.save()
-#         return super().form_valid(form)
-
-# def delete_todo(request, id):
-#     data = get_object_or_404(Todo, id=id)
-#     data.delete()
-#     return redirect("home")
